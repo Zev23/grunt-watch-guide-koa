7fbce7809640b0da3fd22e3cf98d32cecad7af01
Zev <zev.teh@gmail.com>
2014-03-11 13:26:22 +0800

    Changed from localhost to dynamic hostname

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const
   app = koa();
 
 app.use(function *(){
-  this.body = "<script src=\"http://localhost:35729/livereload.js\"></script>";
+  this.body = "<script src=\"http://"+this.host.split(":")[0]+":35729/livereload.js\"></script>";
   this.body += "Hello World\n";
 });
 
